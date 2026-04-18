547233e69eb0bf9c0a28ba6f5ef912e5232b1539
Mohamed ElTahan <mosherifeltahan@gmail.com>
2026-04-18 23:13:27 +0200

    feat: finalize app entry point and integration

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,9 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'core/utils/theme.dart';
+import 'features/calculator/logic/calculator_cubit.dart';
+import 'features/visualization_3d/logic/render_cubit.dart';
+import 'features/calculator/view/calculator_screen.dart';
 
 void main() {
   runApp(const SatelliteApp());
@@ -9,12 +14,18 @@ class SatelliteApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      debugShowCheckedModeBanner: false,
-      home: Scaffold(
-        appBar: AppBar(title: const Text('Satellite')),
-        body: const Center(child: Text('Satellite')),
+    return MultiBlocProvider(
+      providers: [
+        BlocProvider(create: (_) => CalculatorCubit()),
+        BlocProvider(create: (_) => RenderCubit()),
+      ],
+      child: MaterialApp(
+        debugShowCheckedModeBanner: false,
+        title: 'Satellite Tracker',
+        theme: SatelliteTheme.themeData,
+        home: const CalculatorScreen(),
       ),
     );
   }
 }
+
