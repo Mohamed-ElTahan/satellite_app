34e20323e8c60b5184eee5a7f2fefb3320fc4081
Mohamed ElTahan <mosherifeltahan@gmail.com>
2026-04-20 15:41:19 +0200

    feat(calc-logic): implement look-angle trigonometry and state orchestration

--- lib/features/calculator/logic/calculator_state.dart
+++ lib/features/calculator/logic/calculator_state.dart
@@ -1,45 +1,26 @@
 import 'package:equatable/equatable.dart';
 import '../../../../core/models/location_data.dart';
+import '../../../../core/models/satellite_output.dart';
 
 class CalculatorState extends Equatable {
   final LocationData inputs;
-  final double azimuth;
-  final double elevation;
-  final double rangeKm;
-  final double footprintCentralAngleRad;
-  final bool isTrackingISS;
-  final bool signalAcquired;
+  final SatelliteOutput output;
 
   const CalculatorState({
     required this.inputs,
-    this.azimuth = 0.0,
-    this.elevation = 0.0,
-    this.rangeKm = 0.0,
-    this.footprintCentralAngleRad = 0.0,
-    this.isTrackingISS = false,
-    this.signalAcquired = false,
+    this.output = const SatelliteOutput(),
   });
 
   CalculatorState copyWith({
     LocationData? inputs,
-    double? azimuth,
-    double? elevation,
-    double? rangeKm,
-    double? footprintCentralAngleRad,
-    bool? isTrackingISS,
-    bool? signalAcquired,
+    SatelliteOutput? output,
   }) {
     return CalculatorState(
       inputs: inputs ?? this.inputs,
-      azimuth: azimuth ?? this.azimuth,
-      elevation: elevation ?? this.elevation,
-      rangeKm: rangeKm ?? this.rangeKm,
-      footprintCentralAngleRad: footprintCentralAngleRad ?? this.footprintCentralAngleRad,
-      isTrackingISS: isTrackingISS ?? this.isTrackingISS,
-      signalAcquired: signalAcquired ?? this.signalAcquired,
+      output: output ?? this.output,
     );
   }
 
   @override
-  List<Object> get props => [inputs, azimuth, elevation, rangeKm, footprintCentralAngleRad, isTrackingISS, signalAcquired];
+  List<Object> get props => [inputs, output];
 }
